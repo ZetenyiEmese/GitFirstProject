4bd223037ee78132c3ee4b4e8363409f329047db
Szuts Judit <judit.szuts.mob@gmail.com>
2019-04-29 15:59:16 +0200

    a5

--- FirstProject/src/Greet.java
+++ FirstProject/src/Greet.java
@@ -1,7 +1,7 @@
 public class Greet {
 
-  public static void greet(){
-    System.out.println("Greetings!");
+  public static void greet(String greet, String name){
+    System.out.println(greet + ", " + name + "!");
   }
 
 }
