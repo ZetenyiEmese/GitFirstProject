436449367f552f3b263a9e677ff6d492e77965a7
Zétényi Emese <zetenyi.emese@gmail.com>
2019-04-29 14:45:04 +0200

    conflict resolved

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -1,39 +1,22 @@
 public class Main {
 
   public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     greet();
-  }
-
-  public static void greet(){
-    System.out.println("My name is: Judit");
-=======
-=======
->>>>>>> f0dd02f56c308196b7ab6740f5f9e89e7faf6628
-    cheer();
-  }
-
-  public static void cheer(){
-    System.out.println("Hello Backend");
-<<<<<<< HEAD
->>>>>>> f0dd02f56c308196b7ab6740f5f9e89e7faf6628
-=======
->>>>>>> f0dd02f56c308196b7ab6740f5f9e89e7faf6628
-=======
     count();
     cheer();
   }
 
-  public static void cheer() {
+  public static void greet(){
+    System.out.println("My name is: Judit");
+  }
+
+  public static void cheer(){
     System.out.println("Hello Backend");
   }
 
   public static void count() {
     int age = 46;
     System.out.println(age);
->>>>>>> 3278c4e92f84808c167d1d3ffd8d8363914a63a0
   }
 
 }
