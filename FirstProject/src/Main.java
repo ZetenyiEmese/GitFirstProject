42e74aff0c05cabfaf1bef5b4df8b62cc55728a5
Szuts Judit <judit.szuts.mob@gmail.com>
2019-04-29 14:40:22 +0200

    a1

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
 
   public static void main(String[] args) {
+<<<<<<< HEAD
     greet();
   }
 
   public static void greet(){
     System.out.println("My name is: Judit");
+=======
+    cheer();
+  }
+
+  public static void cheer(){
+    System.out.println("Hello Backend");
+>>>>>>> f0dd02f56c308196b7ab6740f5f9e89e7faf6628
   }
 
 }
