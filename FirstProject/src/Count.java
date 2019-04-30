c63652003281e210880310ca3e519824abf67ce7
Agnes <agnes.koperveisz@yahoo.co.uk>
2019-04-30 09:33:32 +0200

    b5

--- FirstProject/src/Count.java
+++ FirstProject/src/Count.java
@@ -1,7 +1,7 @@
 public class Count {
 
-  public static void count(int age) {
-    System.out.println(age);
+  public static void count(int age, String name) {
+    System.out.println(age + name);
   }
 
 }
